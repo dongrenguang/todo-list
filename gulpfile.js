93eda7dfd5fe0e6496722fbfefd6905b7e48ed3b
Renguang Dong <dongrenguang@gmail.com>
2016-09-02 17:55:42 +0800

    Create gulpfile.js.

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require("gulp");
+const webpack = require("webpack");
+const webpackDevServer = require("webpack-dev-server");
+
+gulp.task("default", ["dev"], () => {
+
+});
+
+gulp.task("dev", () => {
+
+});
+
+gulp.task("build", () => {
+
+});
